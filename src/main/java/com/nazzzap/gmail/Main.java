3289a1d2fba7d77722362356e759b75467f46d16
Vladislav Nazarov <nazzzap@gmail.com>
2023-02-19 19:44:21 +0600

    off

--- src/main/java/com/nazzzap/gmail/Main.java
+++ src/main/java/com/nazzzap/gmail/Main.java
@@ -2,7 +2,7 @@ package com.nazzzap.gmail;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("oww");
+        System.out.println("komofkf");
 
     }
 }
