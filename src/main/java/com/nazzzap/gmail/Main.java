9984ae1b86ab57b780c91a264610e2c3121686e6
Vladislav Nazarov <nazzzap@gmail.com>
2023-02-19 18:37:29 +0600

    changes main

--- src/main/java/com/nazzzap/gmail/Main.java
+++ src/main/java/com/nazzzap/gmail/Main.java
@@ -2,6 +2,6 @@ package com.nazzzap.gmail;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my darling");
+        System.out.println("Hello world");
     }
 }
