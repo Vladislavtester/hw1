ae47e24521ee48928f0ac683cf1c40b63eee009a
Vladislavtester <125458866+Vladislavtester@users.noreply.github.com>
2023-02-19 19:16:27 +0600

    Update Main.java

--- src/main/java/com/nazzzap/gmail/Main.java
+++ src/main/java/com/nazzzap/gmail/Main.java
@@ -2,6 +2,6 @@ package com.nazzzap.gmail;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helo world");
-    }
+        System.out.println("Hell world");
+    
 }
